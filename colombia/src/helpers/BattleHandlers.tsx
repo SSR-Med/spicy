1a48863b750614a661ed9d869eca06ea168de32d
LostOni <maimorenovaloyes@gmail.com>
2024-07-23 21:01:11 -0500

    Añadido turno de ataque del jugador y defensa del oponente.

---
+++ colombia/src/helpers/BattleHandlers.tsx
@@ -0,0 +1,7 @@
+export function randomBetween(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export function randomCardIndex(length: number) {
+    return Math.floor(Math.random() * length);
+}
